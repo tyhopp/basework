6b9b2b516f3a1e1d55cc8d57b52a712e0d48b229
Ty Hopp <hopp.ty.c@gmail.com>
2020-04-12 21:52:27 +0800

    Update generate link to basework-example

--- generate/create-new-project.js
+++ generate/create-new-project.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 const createNewProject = async () => {
-  execSync(`git clone https://github.com/tyhopp/site-starter`, {
+  execSync(`git clone https://github.com/tyhopp/basework-example`, {
     stdio: [0, 1, 2], // Show output
     cwd: path.resolve('.'), // Path to resolve the file
   })
